a06dbb0a52d2c1df05cf5ac32c3d811db7fdbd87
Walter Santillan <30912548+Wall404@users.noreply.github.com>
2020-02-19 14:20:05 -0300

    Added Params

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask_restplus import Api, Resource, fields
 
-flask_app = Flask(__name__)
-app = Api(app = flask_app,
+app = Flask(__name__)
+api = Api(app = app,
     version = "1.0", 
-    title = "Web Service API", 
+    title = "Web Service Data API", 
     description = "Mock example of API DATA"
 )
 
-name_space = app.namespace('Flask API', description='Mock Example')
+name_space = api.namespace('Flask API', description='Mock Example')
 
 @name_space.route("/")
+@name_space.param('ejemplo', 'desc')
+@name_space.doc(params={'desc':'algo'})
 class MainClass(Resource):
 	def get(self):
 		return {
 			"status": "Got new data"
 		}
+
+    # @api.response(403, 'Not Authorized')
+    # def post(self, id):
+    #     api.abort(403)
+
 	# def post(self):
 	# 	return {
 	# 		"status": "Posted new data"
 	# 	}
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    app.run(debug=True)
